1f53ca505194c6d581a7324710253a7dd7dc155a
iadler19 <42978499+iadler19@users.noreply.github.com>
2018-09-26 14:05:50 -0400

    Add files via upload

--- src/TalleyCounter.java
+++ src/TalleyCounter.java
@@ -1,4 +1,39 @@
 
-public class TalleyCounter {
+public class TalleyCounter 
+{
+	//instance variables
+ private int count; 
 
-}
+ //constructor
+ public TalleyCounter()
+ 	{
+	count = 0;
+	}
+ public TalleyCounter(int startNum)
+ 	{
+	 count = startNum;
+ 	}
+ 
+ public void click( )
+ 	{
+	 count = count + 1;
+	 //count++
+	 //count+=1
+ 	}
+ 
+ public void unclick()
+ 	{
+	 count--;
+	 //count-=1
+ 	}
+ public void reset()
+ 	{
+	 count = 0;
+ 	}
+ public int getCount()
+ 	{ 
+	 return count;
+ 	}
+} 
+ 
+
